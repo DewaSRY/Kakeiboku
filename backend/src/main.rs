dc33e9005e2ceac8963fbace33ba2619b90e5b34
DewaSRY <sdewa6645@gmail.com>
2026-02-27 21:03:13 +0800

    update: router and load env

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,22 +1,29 @@
-mod state;
-mod models;
 mod dtos;
-mod repositories;
-mod services;
 mod handlers;
+mod models;
+mod repositories;
 mod routes;
+mod services;
+mod state;
 
 use axum::Router;
 use sqlx::PgPool;
 use std::{env, net::SocketAddr};
 use tokio::net::TcpListener;
 
-use state::AppState;
 use routes::user_routes;
+use state::AppState;
 
 #[tokio::main]
 async fn main() {
+    
+    match dotenvy::dotenv() {
+        Ok(path) => println!("Loaded .env from: {}", path.display()),
+        Err(e) => eprintln!("No .env loaded (continuing): {}", e),
+    }
+
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
     let pool = PgPool::connect(&db_url)
         .await
         .expect("Failed to connect to DB");
@@ -27,6 +34,5 @@ async fn main() {
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = TcpListener::bind(addr).await.unwrap();
-
     axum::serve(listener, app).await.unwrap();
 }
